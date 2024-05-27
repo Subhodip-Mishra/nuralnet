62fe9d406ca1754c66cf8d542d799b95212df833
Suvodeep Mishra <subhodipmishra47@gmail.com>
2024-05-27 18:24:15 +0530

    d

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 import MobileNav from "@/components/MobileNav";
 import Navber from "@/components/Navber";
 import Sidebar, { SideBarProvider } from "@/components/Sidebar";
-import Sidebar1 from "@/components/Sidebar1";
+// import Sidebar1 from "@/components/Sidebar1";
 import { Toaster } from "@/components/ui/toaster";
 import { getApiLimitCount } from "@/lib/api-limit";
 
@@ -13,7 +13,7 @@ const DashBoardLayout = async ({ children }: { children: React.ReactNode }) => {
       <div className="h-full">
         <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 bg-gray-900">
           <Sidebar apiLimitCount={apiLimitCount}>
-            <Sidebar1 apiLimitCount={apiLimitCount} />
+            {/* <Sidebar1 apiLimitCount={apiLimitCount} /> */}
           </Sidebar>
         </div>
         <main className="md:pl-72">
