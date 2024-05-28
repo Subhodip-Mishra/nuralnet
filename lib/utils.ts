d9ae82241e6c7b25170f7172a19dea2b11461823
Suvodeep Mishra <subhodipmishra47@gmail.com>
2024-05-28 10:23:13 +0530

    changes

--- lib/utils.ts
+++ lib/utils.ts
@@ -94,20 +94,20 @@ export const debounce = (func: (...args: any[]) => void, delay: number) => {
 };
 
 // GE IMAGE SIZE
-export type AspectRatioKey = keyof typeof aspectRatioOptions;
-export const getImageSize = (
-  type: string,
-  image: any,
-  dimension: "width" | "height"
-): number => {
-  if (type === "fill") {
-    return (
-      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
-      1000
-    );
-  }
-  return image?.[dimension] || 1000;
-};
+// export type AspectRatioKey = keyof typeof aspectRatioOptions;
+// export const getImageSize = (
+//   type: string,
+//   image: any,
+//   dimension: "width" | "height"
+// ): number => {
+//   if (type === "fill") {
+//     return (
+//       aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+//       1000
+//     );
+//   }
+//   return image?.[dimension] || 1000;
+// };
 
 // DOWNLOAD IMAGE
 export const download = (url: string, filename: string) => {
