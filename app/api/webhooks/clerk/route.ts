3179b189726f10512e140ae2b44c63310f3dae2d
Suvodeep Mishra <subhodipmishra47@gmail.com>
2024-05-29 11:59:18 +0530

    sd

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -15,6 +15,7 @@ export async function POST(req: Request) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
+    
   }
 
   // Get the headers
