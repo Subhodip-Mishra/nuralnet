ff9d17d9497a2907622d1aa8c948610a380c8c77
Suvodeep Mishra <subhodipmishra47@gmail.com>
2024-05-29 13:33:38 +0530

    d

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -15,6 +15,7 @@ export const increaseApiLimit = async () => {
         },
     });
 
+
     if (userApiLimit) {
         await prismadb.userApiLimit.update({
             where: { userId: userId },
