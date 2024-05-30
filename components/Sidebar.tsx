4fe2e7edbff8728a5a4d0f44fc45ca305f4d7bd1
Suvodeep Mishra <subhodipmishra47@gmail.com>
2024-05-30 14:48:51 +0530

    dsf

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -88,7 +88,7 @@ const Sidebar: React.FC<SidebarProps> = ({  children }) => {
                 const isActive = link.route === pathname
 
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? "bg-gradient-to-r from-yellow-300 to-orange-500  text-white" : "text-gray-700"
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? "bg-gradient-to-r from-yellow-400 to-yellow-400 via-orange-400  text-white" : "text-black"
                     }`}>
                     <Link className='sidebar-link' href={link.route}>
                       <Image
@@ -110,7 +110,7 @@ const Sidebar: React.FC<SidebarProps> = ({  children }) => {
                 const isActive = link.route === pathname
 
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? "bg-purple-gradient text-white" : "text-gray-700"
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? "bg-gradient-to-r from-yellow-400 to-yellow-400 via-orange-400 text-white" : "text-black"
                     }`}>
                     <Link className='sidebar-link' href={link.route}>
                       <Image
@@ -125,7 +125,7 @@ const Sidebar: React.FC<SidebarProps> = ({  children }) => {
                   </li>
                 )
               })}
-              <li className='flex-center cursor-pointer gap-2 p-4'>
+              <li className='flex-center cursor-pointer gap-2 p-3'>
                 <UserButton afterSignOutUrl='/' showName />
               </li>
             </ul>
